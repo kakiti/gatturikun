ebb4eb6b183aae9f60e5277f64dc2c77fdfce903
kakiti <kakizaki3315chi@gmail.com>
2020-12-21 18:59:45 +0900

    新規顧客登録ページ作成

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,20 @@
 class Client < ApplicationRecord
+  with_options presence: true do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :tel_number, format: { with: /\A\d{1,11}\z/ }
+    validates :date
+    validates :user_id
+  end
+    
+  validates :prospect_id, numericality: { other_than: 1 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prospect
 end
---
+++ app/models/prospect.rb
@@ -0,0 +1,12 @@
+class Prospect < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '◎(成約)' },
+    { id: 3, name: '○(見込みあり)' },
+    { id: 4, name: '△(微妙)' },
+    { id: 5, name: '×(不成約)' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :clients
+  end
